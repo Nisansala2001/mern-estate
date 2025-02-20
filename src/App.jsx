dfd9aa6d2d248651459b60dc00a54b0d8bfe1eca
Nisansala Rasanjali <IT22547774@my.sliit.lk>
2025-02-20 22:18:14 +0530

    install react js and tailwind css and create the first template

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1 className="text-pink-500">Hello, World!</h1>
+        </div>
+    );
+};
+
+export default App; // This line is crucial for default export
