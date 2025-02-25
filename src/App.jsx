7d3a6ef50fc60a305c3b6273fdeb1a98d29c423d
Nisansala Rasanjali <IT22547774@my.sliit.lk>
2025-02-25 09:31:33 +0530

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"; 
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
-import SingOut from "./pages/SingOut";
+import SignOut from "./pages/SignOut"; // Correctly fixed typo
 import About from "./pages/About";
 import Profile from "./pages/Profile";
-export const App = () => {
-    return <BrowserRouter>
-    
-    <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sing-out" element={<SingOut />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/about" element={<About />} />
+import Header from "./components/Header"; // Correctly fixed typo
 
-    </Routes>
-    
-    </BrowserRouter>;
-}
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Header /> {/* Use the Header component here */}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/sign-out" element={<SignOut />} /> {/* Correctly fixed typo */}
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
-export default App; // This line is crucial for default export
+export default App; // Correctly exporting the App component
