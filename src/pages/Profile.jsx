083654aada312e310248e7adfc3b6367a9e85046
Nisansala Rasanjali <IT22547774@my.sliit.lk>
2025-03-15 22:32:52 +0530

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,1 @@
-// Profile.jsx
-import React from 'react';
 
-const Profile = () => {
-    return (
-        <div>
-            <h1>User Profile</h1>
-            {/* Your profile content here */}
-        </div>
-    );
-};
-
-export default Profile;
