71a9bde9f90990b2821e56de5e6848599a80e543
Nisansala Rasanjali <IT22547774@my.sliit.lk>
2025-03-20 23:32:31 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 // src/App.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom"; 
 import Home from "./pages/Home";
+
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp"; 
 import About from "./pages/About";
@@ -9,15 +10,19 @@ import Header from "./components/Header";
 
 const App = () => {
     return (
+        
         <BrowserRouter>
             <Header /> {/* Use the Header component here */}
             <Routes>
+                
                 <Route path="/" element={<Home />} />
                 <Route path="/sign-in" element={<SignIn />} />
                 <Route path="/sign-up" element={<SignUp />} /> {/* Correctly fixed typo */}
                 <Route path="/profile" element={<Profile />} />
                 <Route path="/about" element={<About />} />
+            
             </Routes>
+            
         </BrowserRouter>
     );
 };
