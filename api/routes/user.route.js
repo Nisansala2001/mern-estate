a2c7eece9297c1583e6aa27a581484eca0193bd0
Nisansala Rasanjali <IT22547774@my.sliit.lk>
2025-03-26 23:30:11 +0530

    Update user.route.js

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
+
 import { test } from '../controllers/user.controller.js'; // Add `.js` extension
 
 const router = express.Router();
 
 router.get('/test', test);
+
 
 export default router;
