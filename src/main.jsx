991632bb73dec26c14306422407aff1c90073b57
Nisansala Rasanjali <IT22547774@my.sliit.lk>
2025-05-14 23:40:57 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,11 @@
 // src/main.jsx
 import React from 'react';
-
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx'; // Make sure this path is correct
-
 import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    
-    <React.StrictMode>
-        
+      <React.StrictMode>
         <App />
     </React.StrictMode>
 );
