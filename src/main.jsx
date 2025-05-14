fb2635f805ea9dd729178be8df096403b048ce8f
Nisansala Rasanjali <IT22547774@my.sliit.lk>
2025-05-14 23:40:33 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx'; // Make sure this path is correct
 
 import './index.css';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     
